dc3e0c1b81a460999c907cd5b0552e88b128da38
shonabotbr32 <shonabots@gmail.com>
2026-04-14 01:51:24 +0530

    Update __init__.py

--- anony/helpers/__init__.py
+++ anony/helpers/__init__.py
@@ -5,6 +5,7 @@
 
 from ._admins import admin_check, can_manage_vc, is_admin, reload_admins
 from ._dataclass import Media, Track
+from ._exec import format_exception, meval
 from ._inline import Inline
 from ._queue import Queue
 from ._thumbnails import Thumbnail
